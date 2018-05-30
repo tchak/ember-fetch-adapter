085687878b2fb5af94c9a439e5c6a2d088360f53
Paul Chavard <git@paul.chavard.net>
2018-05-30 16:06:53 +0200

    Add ember-fetch in the consumer app

---
+++ blueprints/ember-fetch-adapter/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  description: 'Add ember-fetch in the consumer app',
+
+  normalizeEntityName() {},
+
+  /*
+  * This is necessary until this is fixed
+  * https://github.com/ember-cli/ember-fetch/issues/98
+  */
+  afterInstall() {
+    return this.addPackagesToProject([
+      { name: 'ember-fetch', target: '^4.0.2' }
+    ]);
+  }
+};
